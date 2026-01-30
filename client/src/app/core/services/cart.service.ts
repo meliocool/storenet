735153c554d68f02bcf296c833d5813b8af3f57b
melio <dhitanimam05@gmail.com>
2026-01-30 14:31:23 +0700

    feat: shopping cart functionality, signals, quantity modifier, client side redis

--- client/src/app/core/services/cart.service.ts
+++ client/src/app/core/services/cart.service.ts
@@ -15,6 +15,20 @@ export class CartService {
   itemCount = computed(() => {
     return this.cart()?.items.reduce((sum, item) => sum + item.quantity, 0);
   });
+  totals = computed(() => {
+    const cart = this.cart();
+    if (!cart) return null;
+    const subTotal = cart.items.reduce((sum, item) => sum + item.price * item.quantity, 0);
+    // * TODO tambahkan nanti
+    const shipping = 0;
+    const discount = 0;
+    return {
+      subTotal,
+      shipping,
+      discount,
+      total: subTotal + shipping - discount,
+    };
+  });
 
   getCart(id: string) {
     return this.http.get<Cart>(this.baseUrl + 'cart?id=' + id).pipe(
@@ -38,6 +52,33 @@ export class CartService {
     }
     cart.items = this.AddOrUpdateItem(cart.items, item, quantity);
     this.setCart(cart);
+  }
+
+  removeItemFromCart(productId: number, quantity = 1) {
+    const cart = this.cart();
+    if (!cart) return;
+    const index = cart.items.findIndex((x) => x.productId === productId);
+    if (index != -1) {
+      if (cart.items[index].quantity > quantity) {
+        cart.items[index].quantity -= quantity;
+      } else {
+        cart.items.splice(index, 1);
+      }
+      if (cart.items.length === 0) {
+        this.deleteCart();
+      } else {
+        this.setCart(cart);
+      }
+    }
+  }
+
+  deleteCart() {
+    this.http.delete(this.baseUrl + 'cart?id=' + this.cart()?.id).subscribe({
+      next: () => {
+        localStorage.removeItem('cart_id');
+        this.cart.set(null);
+      },
+    });
   }
 
   private AddOrUpdateItem(items: CartItem[], item: CartItem, quantity: number): CartItem[] {
